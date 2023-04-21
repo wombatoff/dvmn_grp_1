dc0157e22805f0e13b0b3a0954c6179194cd982e
Wombatoff <mwombatoff@gmail.com>
2023-04-21 18:53:03 +0300

    minor refactoring templates

--- storage/urls.py
+++ storage/urls.py
@@ -7,6 +7,6 @@ app_name = 'storage'
 urlpatterns = [
     path('my-rent/', views.my_rent, name='my_rent'),
     path('boxes/', views.storages, name='storages'),
-    path('boxes/<int:storage_id>/', views.boxes, name='boxes'),
+    path('storages/<int:storage_id>/', views.boxes, name='boxes'),
     path('faq/', views.faq, name='faq'),
 ]
